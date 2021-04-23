e101a2617811b2d96cfbd8d8073987c32f1d2a0a
HelenePiet <helene.m.piet@gmail.com>
2021-04-22 19:47:41 -0700

    pacho movement emulated

--- Screen.py
+++ Screen.py
@@ -10,17 +10,23 @@ class Screen:
         self.window.fill(background_color)  # screen background color
         pygame.display.set_caption("Pachokoban")    # sets window caption
 
-    def shows(self, tile, position):
+    def shows(self, tile, position, background_color):
         tile_size = 50
         
         if tile == 'w':
             tile_image = pygame.image.load("tiles/wall-yellow.png")
             self.window.blit(tile_image, (position[1]*tile_size, position[0]*tile_size))
             pygame.display.update() # only update portions of the screen
-            pygame.display.flip()    # updates the entire screen with all the tiles
+            pygame.display.flip()    # updates the entire screen with all the tiles    
 
         elif tile == 's':
             tile_image = pygame.image.load("tiles/spot-yellow.png")
+            self.window.blit(tile_image, (position[1]*tile_size, position[0]*tile_size))
+            pygame.display.update()
+            pygame.display.flip()
+
+        elif tile == 'p':
+            tile_image = pygame.image.load("tiles/pacho.png")
             self.window.blit(tile_image, (position[1]*tile_size, position[0]*tile_size))
             pygame.display.update()
             pygame.display.flip()
@@ -31,9 +37,11 @@ class Screen:
             pygame.display.update()
             pygame.display.flip()
 
-        elif tile == 'p':
-            tile_image = pygame.image.load("tiles/pacho.png")
-            self.window.blit(tile_image, (position[1]*tile_size, position[0]*tile_size))
+        elif tile == 'b':   # background tile
+            #background_color=(0,0,0)
+            rect=(position[1]*tile_size, position[0]*tile_size, tile_size, tile_size)
+            print(rect)
+            pygame.draw.rect(self.window,background_color,rect)
             pygame.display.update()
             pygame.display.flip()
 
--- game.py
+++ game.py
@@ -12,14 +12,15 @@ pygame.init()       # Initializes the display as well as other things
 levelfile = json.load(open("level_001.json"))
 
 ## Initialize objects
-screen = Screen(350, 350, background_color=[255,228,182])   # screen width and height are in px
+background_color=(255,228,182)
+screen = Screen(350, 350, background_color)   # screen width and height are in px
 
 # Display first level
 for i in range(0,len(levelfile[0])-1):    # customize this so we don't use integers
     for j in range(0,len(levelfile)):
         position = (i,j)    # save object position
         tile = levelfile[i][j]     # read object to draw
-        screen.shows(tile, position)   # show objects on screen
+        screen.shows(tile, position, background_color)   # show objects on screen
 
         # Initializes pacho object with its position
         if tile == 'p':
@@ -32,8 +33,9 @@ while True:
         # If user presses keyboard touch
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                pacho.modifyPosition(x_move=0, y_move=-1)
-                screen.shows('p', pacho.position)
+                screen.shows('b', pacho.position, background_color) # draws background tile on top of pacho
+                pacho.modifyPosition(x_move=0, y_move=-1)  # modifies pacho's position 
+                screen.shows('p', pacho.position, background_color) # draws pacho at new position
         
         if event.type == QUIT:          # Keeps pygame window open unless asked otherwise
             pygame.quit()
